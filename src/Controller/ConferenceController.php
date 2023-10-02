8d7ab2559e8a2929b4d692be86a58e602c704f0f
RAKOTONDRABE_Ph <p.rakotondrabe@lprs.fr>
2023-10-02 11:00:21 +0200

    #4 ADD - ajout des fichiers nécessaires pour le fonctionnement des CRUD

---
+++ src/Controller/ConferenceController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Conference;
+use App\Form\ConferenceType;
+use App\Repository\ConferenceRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/conference')]
+class ConferenceController extends AbstractController
+{
+    #[Route('/', name: 'app_conference_index', methods: ['GET'])]
+    public function index(ConferenceRepository $conferenceRepository): Response
+    {
+        return $this->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_conference_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $conference = new Conference();
+        $form = $this->createForm(ConferenceType::class, $conference);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($conference);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_conference_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('conference/new.html.twig', [
+            'conference' => $conference,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_conference_show', methods: ['GET'])]
+    public function show(Conference $conference): Response
+    {
+        return $this->render('conference/show.html.twig', [
+            'conference' => $conference,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_conference_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Conference $conference, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ConferenceType::class, $conference);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_conference_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('conference/edit.html.twig', [
+            'conference' => $conference,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_conference_delete', methods: ['POST'])]
+    public function delete(Request $request, Conference $conference, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$conference->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($conference);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_conference_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/ConferenceType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Conference;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ConferenceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('description')
+            ->add('date')
+            ->add('heure')
+            ->add('duree')
+            ->add('amphitheatre')
+            ->add('ref_user')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Conference::class,
+        ]);
+    }
+}
