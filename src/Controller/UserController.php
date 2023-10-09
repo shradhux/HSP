be6eec30899d559ddbd1827b62da53ccfe9fe10b
KALLEE-DIGUMBER_Sh <s.kallee-digumber@lprs.fr>
2023-10-09 12:21:47 +0200

    In progress theme et fonctionnalite de base #7

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\User1Type;
+use App\Form\UserType;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
